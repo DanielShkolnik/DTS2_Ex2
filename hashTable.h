eabeb73244afa46068f2371522a806d405e36ccf
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2020-06-04 15:11:51 +0300

    new

--- hashTable.h
+++ hashTable.h
@@ -13,6 +13,8 @@ private:
     Avl<int,Artist>** arr;
     int numOfUsedCells;
     int arrSize;
+
+    void initArr(Avl<int,Artist>** array, int size);
     void increaseSize();
     void decreaseSize();
     void deleteArr();
@@ -28,6 +30,49 @@ public:
     Artist* findArtist(int artistID);
 };
 //void addToArr(int index, int server_id, int DC_id, ChainNode** arr);
+
+void HashTable::initArr(Avl<int,Artist>** array, int size){
+    for(int i=0; i<size; i++){
+        array[i] = new Avl<int,Artist>;
+    }
+}
+
+
+class ArtistPredicate{
+public:
+    void operator()(Node<int,Artist>* artistNode){
+        // get the root of song tree of the current artist
+        Node<int,Song>* songNode = artistNode->getData();
+
+        // do postorder to free data (song) in each node
+        SongPredicate songDelete;
+
+        postorder<int,Song,SongPredicate>(songNode,songDelete);
+    }
+    explicit ArtistPredicate() = default;
+    ArtistPredicate(const ArtistPredicate& a) = delete;
+};
+
+
+
+
+void HashTable::decreaseSize(){
+    int prevSize = this->arrSize;
+    int newSize = this->arrSize/2;
+    Avl<int,Artist>** newArr = new Avl<int,Artist>*[newSize];
+    initArr(newArr,newSize);
+    for(int i=0; i<prevSize; i++){
+        Node<int,Artist>* artistNode = this->arr[i]->getRoot();
+
+        ArtistPredicate artistPredicate;
+
+        postorder<int,Artist,ArtistPredicate>(artistNode,artistPredicate);
+    }
+    delete[] this->arr;
+    this->size = new_size;
+    this->arr = newArr;
+};
+
 
 
 #endif //DTS2_EX2_HASHTABLE_H
